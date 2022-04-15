4f3cfbff1fbe4fc1618b0f0760bf9d20bad555a0
Jaquavia Walker <jaquaviawalker@gmail.com>
2022-04-15 17:18:55 -0400

    ex 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+	let splitString = string.split("");
+	let reverseArray = splitString.reverse();
+	let joinArray = reverseArray.join("");
+	return joinArray;
 };
+
+reverseString("hello");
+reverseString("hello there");
+reverseString("123! abc!");
+reverseString("hello");
+reverseString("");
 
 // Do not edit below this line
 module.exports = reverseString;
