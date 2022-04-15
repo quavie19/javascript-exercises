c4b62990d33804540a85701c51b39c1f7d5da978
Jaquavia Walker <jaquaviawalker@gmail.com>
2022-04-15 15:42:51 -0400

    ex 1  & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+	let repeatedString = "";
 
+	if (num === 0) return "";
+	else if (num < 0) return "ERROR";
+	for (i = 0; i < num; i++) {
+		repeatedString += string;
+	}
+	return repeatedString;
 };
+console.log(repeatString("hey", 3));
+console.log(repeatString("hey", 10));
+console.log(repeatString("hey", 1));
+console.log(repeatString("hey", 0));
+console.log(repeatString("hey", -1));
+console.log(repeatString("hey", Math.floor(Math.random() * 15)));
+console.log(repeatString("", 10));
 
-// Do not edit below this line
+// Do not edit below th is line
 module.exports = repeatString;
